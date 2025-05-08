1ddbc9b85689f61403a357dee21b13d76149f170
Bruno Januário <bruno.januario1998@gmail.com>
2025-05-08 15:53:56 +0100

    feat: add endpoint to get photos

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file, abort, send_from_directory
 import retrieval
+import os
+
+IMAGE_FOLDER = 'images'
 
 app = Flask(__name__)
 
@@ -17,6 +20,22 @@ def search_images():
         return jsonify({"results": results})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+
+@app.route('/image', methods=['GET'])
+def get_image():
+    filename = request.args.get('filename')  # get from query string
+
+    if not filename:
+        return abort(400, description="Missing 'filename' parameter")
+
+    image_path = os.path.join(IMAGE_FOLDER, filename)
+
+    if os.path.exists(image_path):
+        return send_file(image_path, mimetype='image/jpeg')
+    else:
+        return abort(404, description="Image not found")
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
